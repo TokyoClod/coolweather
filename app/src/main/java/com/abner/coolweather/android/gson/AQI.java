b8b2b934de84a05c3d953d8c20a0bf85396263ba
abner-win <793907563@qq.com>
2018-06-25 10:40:38 +0800

    加入显示天气的功能

---
+++ app/src/main/java/com/abner/coolweather/android/gson/AQI.java
@@ -0,0 +1,9 @@
+package com.abner.coolweather.android.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
--- app/src/main/java/com/abner/coolweather/android/util/Utility.java
+++ app/src/main/java/com/abner/coolweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.abner.coolweather.android.db.City;
 import com.abner.coolweather.android.db.County;
 import com.abner.coolweather.android.db.Province;
+import com.abner.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
@@ -69,5 +71,17 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
